df71b495a1491cd9312d92d9c0ddf12526561125
adiEdelsberg <edelsberg02@gmail.com>
2015-03-14 21:01:18 +0200

    add search filter

--- snapDish/www/js/controllers.js
+++ snapDish/www/js/controllers.js
@@ -1,30 +1,38 @@
 angular.module('controllers', ['services'])
 
 .controller('LoginCtrl', ['$scope', '$location', function($scope, $location) {
-  
+
   	$scope.fbLogin = function() {
-	
+
      openFB.login(
         function(response) {
             if (response.status === 'connected') {
-                 $scope.$apply(function(){ 
-					$location.path('restaurants'); 
-				}); 
-              
+                 $scope.$apply(function(){
+					$location.path('restaurants');
+				});
+
             } else {
                 alert('Facebook login failed');
             }
         },
-        {scope: 'email'});  
+        {scope: 'email'});
 }
-  
-  
+
+
 }])
 
 .controller('RestaurantsCtrl', ['$scope', 'restaurants', function($scope, restaurants){
+    $scope.query = '';
+    restaurants.getData().then(function(response){
+      $scope.restaurants = response.data;
+    });
 
-  restaurants.getData().then(function(response){
-    $scope.restaurants = response.data;
-  });
+    $scope.search = function (row) {
+       console.log('b');
+        return (angular.lowercase(row.NAME).indexOf($scope.query || '') !== -1 );
+    };
+    $scope.setList = function (query) {
+        $scope.query = query;
+    };
 
 }]);
