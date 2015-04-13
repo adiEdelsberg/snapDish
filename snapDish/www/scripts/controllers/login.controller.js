84d1b5ed19207c817b3a85518b98c59670b1551e
Sergey Krohin <sergeykrohin86@gmail.com>
2015-04-13 21:56:04 +0300

    users service(not finished)

--- snapDish/www/scripts/controllers/login.controller.js
+++ snapDish/www/scripts/controllers/login.controller.js
@@ -1,10 +1,25 @@
 angular.module('app')
 
-.controller('LoginCtrl', ['$scope', '$location', function($scope, $location) {
+.controller('LoginCtrl', ['$scope', '$location', 'user', function($scope, $location, user) {
 
 $scope.fbLogin = function() {
 
 openFB.login( function(response) {
+
+	openFB.api({
+		path: '/me',
+        params: {fields: 'id, name, email, picture'},
+        success: function(fbUser) {
+            
+        	user.setCurrentUser(fbUser);
+ 
+        },
+        error: function(error) {
+
+            alert('Facebook error: ' + error.error_description);
+
+        }
+    });
 
   if (response.status === 'connected') {
 
---
+++ snapDish/www/scripts/services/user.service.js
@@ -0,0 +1,27 @@
+angular.module('app')
+
+.factory('user', [function() {
+
+    //cached dish
+    var currentUser;
+
+    var methods = {
+
+      getCurrentUser: function() {
+
+        return currentUser;
+
+      },
+
+      setCurrentUser: function(user) {
+
+        currentUser = user;
+
+      }
+
+    }
+
+
+return methods;
+
+}]);
