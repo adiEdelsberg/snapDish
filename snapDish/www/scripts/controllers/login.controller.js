30208293c38fd1f60dd2c774bd72df1214f9b3f5
Sergey Krohin <sergeykrohin86@gmail.com>
2015-04-25 18:41:14 +0300

    single sign on

--- snapDish/www/scripts/controllers/login.controller.js
+++ snapDish/www/scripts/controllers/login.controller.js
@@ -1,7 +1,31 @@
 angular.module('app')
 
-.controller('LoginCtrl', ['$scope', '$location', 'user', function($scope, $location, user) {
+.controller('LoginCtrl', ['$scope', '$state', '$ionicLoading', 'user', function($scope, $state, $ionicLoading, user) {
 
-	
+	$scope.login = function () {
+
+		$ionicLoading.show({
+			template: '<i class="ion-loading-c"></i><br/>Login...'
+		});
+
+		if (!window.cordova) {
+			var appId = prompt("Enter FB Application ID", "");
+			facebookConnectPlugin.browserInit("1625788814323152");
+		}
+		facebookConnectPlugin.login( ["email"],
+			function (response) { 
+
+				$ionicLoading.hide();
+
+				$state.go('restaurants'); 
+
+			},
+			function (response) { 
+
+				alert(JSON.stringify(response));
+
+			});
+
+	}
 
 }])
